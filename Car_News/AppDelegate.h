fec4a2126ce47e83d77b443ca62d37556d53a3a1
caohongyang <caohongyang@hongyang.local>
2016-04-15 19:57:00 +0800

    Initial Commit

---
+++ Car_News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Car_News
+//
+//  Created by caohongyang on 16/4/15.
+//  Copyright © 2016年 caohongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Car_News/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Car_News
+//
+//  Created by caohongyang on 16/4/15.
+//  Copyright © 2016年 caohongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
